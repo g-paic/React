bb60752059d0822ecd7c6687469e8a7c66327833
Gabix123 <gabriel.paic@fer.hr>
2025-11-10 11:20:38 +0100

    section-18

---
+++ Section 18 - Handling Forms via Form Actions/Starting Project/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header.jsx';
+import SignUp from './components/SignUp.jsx';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main>
+        <SignUp/>
+      </main>
+    </>
+  );
+}
+
+export default App;
